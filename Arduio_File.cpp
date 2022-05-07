28c3a62e76ca889a54239573667afc4267ab00de
robin.petit <mail@robinpetit.fr>
2022-05-07 13:46:44 +0200

    end to nsi project

--- Arduio_File.cpp
+++ Arduio_File.cpp
@@ -3,30 +3,35 @@
 #include <Servo.h>
 
 Servo servo;
+
 int Pin_Servo = 9;
-int Pin_Open_State = 2;
-int Pin_Close_State = 4;
+int Pin_State = 2;
 void setup (){
+  //on définie le pin utilisé pour envoyer le signal au servo moteur
   servo.attach(Pin_Servo);
-  pinMode(Pin_Open_State,INPUT);
-  pinMode(Pin_Close_State,INPUT);
+  //on définie le pin utilisé par le raspberry pi pour envoyer le signal (en mode lecture)
+  pinMode(Pin_State,INPUT);
   Serial.begin(9600);
 }
 
 void open(){
+  //On tourne de 90°
   servo.write(90);
 }
 
 void close(){
+  //On tournede de -90°
   servo.write(0);
 }
 void loop(){
-  int open_state = digitalRead(Pin_Open_State);
-  int close_state = digitalRead(Pin_Close_State);
-  if (open_state == HIGH){
+  //on récupère le signal envoyer par le raspberry pi
+  int state = digitalRead(Pin_State);
+  //il y a un signal, ou le courant est haut, alors on ouvre
+  if (state == HIGH){
     open();
-  }
-  if (close_state == HIGH){
-    close();
-  }
+    }
+    //il n'y a pas de signal, ou le courant est bas, alors on ferme
+   if (state == LOW){
+      close();
+    }
 }
